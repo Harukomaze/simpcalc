181557551c8c9a59f4654354ffd4d7fe1e66138a
Harukomaze <85628915+Harukomaze@users.noreply.github.com>
2021-06-28 12:00:18 -0400

    Create discord.py

---
+++ examples/discord.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from simpcalc import Calculate
+
+class Calc(commands.Cog):
+	  def __init__(self, bot):
+		    self.bot = bot
+        self.calculator = Calculate()
+        
+    @commands.command()
+    async def calculate(self, ctx, *, expr = None):
+        if not expr:
+          return await ctx.send('You have to provide an equation to calculate!')
+        result = self.calculator.calculate(expr)
+        await ctx.send(f'The result is {result}')
+        
+        
+def setup(bot):
+  bot.add_cog(Calc(bot))
+  
