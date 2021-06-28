4640091b7b1d22293d451c41bce6f1828d4cedf7
Harukomaze <85628915+Harukomaze@users.noreply.github.com>
2021-06-28 18:22:49 -0400

    Update discord.py

--- examples/discord.py
+++ examples/discord.py
@@ -2,12 +2,12 @@ from discord.ext import commands
 from simpcalc import simpcalc
 
 class Calc(commands.Cog):
-	  def __init__(self, bot):
-		    self.bot = bot
+     def __init__(self, bot):
+	self.bot = bot
         self.calculator = simpcalc.Calculate()
         
-    @commands.command()
-    async def calculate(self, ctx, *, expr = None):
+     @commands.command()
+     async def calculate(self, ctx, *, expr = None):
         if not expr:
           return await ctx.send('You have to provide an equation to calculate!')
         result = await self.calculator.calculate(expr)
