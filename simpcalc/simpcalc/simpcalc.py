f0603e7d192cbd474a1c3e127007e07ad81b48d7
Harukomaze <85628915+Harukomaze@users.noreply.github.com>
2021-06-28 12:02:00 -0400

    Add files via upload

---
+++ simpcalc/simpcalc/simpcalc.py
@@ -0,0 +1,22 @@
+import asyncio
+import aiohttp
+import urllib.parse
+from errors import Overflow, BadArgument
+
+
+class Calculate:
+    def __init__(self):
+        pass
+
+    async def calculate(self, expr):
+        expr = expr.replace('**', '^')
+        my_expr = "".join(expr.split(expr))
+        my_expr = urllib.parse.quote(expr)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"http://api.mathjs.org/v4/?expr={my_expr}") as r:
+              result = await r.text()
+        if result.startswith("Error:"):
+            raise BadArgument("An Invalid argument was passed.")
+        return result
+        
+        
