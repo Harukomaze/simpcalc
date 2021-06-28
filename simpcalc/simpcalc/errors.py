8c631cf789c45288f04741dadeb1b36911ea6c1d
Harukomaze <85628915+Harukomaze@users.noreply.github.com>
2021-06-28 12:02:49 -0400

    Delete simpcalc/simpcalc directory

--- simpcalc/simpcalc/errors.py
+++
@@ -1,6 +0,0 @@
-class Overflow(Exception):
-    """This is raised when a 'too large' value is passed"""
-    pass
-class BadArgument(Exception):
-    """This is raise when a bad/invalid argument is passed"""
-    pass
